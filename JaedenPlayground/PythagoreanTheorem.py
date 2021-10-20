9ab8273f3ff1b4298628a023270728ec8f73adaa
BestBlockBuster <jaedengeertsema@gmail.com>
2021-10-20 16:38:10 -0400

    spelling

--- JaedenPlayground/PythagoreanTheorem.py
+++ JaedenPlayground/PythagoreanTheorem.py
@@ -1,21 +1,21 @@
 import math
 def Pith():
-    print('1. Calculate for Hyptonuse Length')
+    print('1. Calculate for Hypotenuse Length')
     print('2. Calculate for Side Length')
     A = float(input(''))
 
     if A == 1:
-        print('Give Hight')
+        print('Give Height')
         H = float(input(''))
-        print('Give Lenght')
+        print('Give Length')
         L = float(input(''))
 
         print(math.sqrt(H*H+L*L),'=S')
 
     elif A == 2:
-        print('Give Slant Hight')
+        print('Give Slant Height')
         S = float(input(''))
-        print('Give Lenght')
+        print('Give Length')
         L = float(input(''))
 
         print(math.sqrt(S*S-L*L),'=H')
