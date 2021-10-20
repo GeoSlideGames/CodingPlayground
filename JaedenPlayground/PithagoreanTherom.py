4c4570a3afeac2d94ab9ad1b1a0719c6bf13c3c9
BestBlockBuster <jaedengeertsema@gmail.com>
2021-10-19 21:23:56 -0300

    Took awy exess

--- JaedenPlayground/PithagoreanTherom.py
+++ JaedenPlayground/PithagoreanTherom.py
@@ -1,9 +1,8 @@
 def Pith():
     print('1. H^2 + L^2 = S^2')
     print('2. S^2 - L^2 = H^2')
-    print('3. S^2 - H^2 = L^2')
 
-    A = float(input('Select option 1,2 or 3'))
+    A = float(input('Select option 1 or 2:'))
 
     if A == 1:
         H = float(input('Give Hight:'))
@@ -14,11 +13,5 @@ def Pith():
         S = float(input('Give Slant Hight:'))
         L = float(input('Give Lenght:'))
         print(S*S-L*L,'=H^2')
-        print()
-    elif A == 3:
-        S = float(input('Give Slant Hight:'))
-        H = float(input('Give Hight:'))
-        
-        print(S*S-H*H,'=L^2')
         print()
 Pith()
