f189a783ce414030f11dfd38ab2d12cae17ce3ea
BestBlockBuster <jaedengeertsema@gmail.com>
2021-10-19 21:40:06 -0300

    Did sqrt function

--- JaedenPlayground/PithagoreanTherom.py
+++ JaedenPlayground/PithagoreanTherom.py
@@ -1,3 +1,4 @@
+import math
 def Pith():
     print('1. H^2 + L^2 = S^2')
     print('2. S^2 - L^2 = H^2')
@@ -7,11 +8,11 @@ def Pith():
     if A == 1:
         H = float(input('Give Hight:'))
         L = float(input('Give Lenght:'))
-        print(H*H+L*L,'=S^2')
+        print(math.sqrt(H*H+L*L),'=S')
         print()
     elif A == 2:
         S = float(input('Give Slant Hight:'))
         L = float(input('Give Lenght:'))
-        print(S*S-L*L,'=H^2')
+        print(math.sqrt(S*S-L*L),'=H')
         print()
 Pith()
